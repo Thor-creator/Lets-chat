b0847ff00af16ad38399b143d34f2686b3acd67b
Thor-creator <69631224+Thor-creator@users.noreply.github.com>
2022-02-03 19:46:16 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -17,10 +17,33 @@ const firebaseConfig = {
       firebase.initializeApp(firebaseConfig);
     //const analytics = getAnalytics(app);
 
+    user_name = localStorage.getItem("user_name")
+    document.getElementById("welcome").innerHTML = "Welcome " + user_name + "!"
+
+    function addroom(){
+      room_name = document.getElementById("room_name").value
+      localStorage.setItem("room_name", room_name)
+      firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room"
+        
+      })
+      window.location = "kwitter_page.html"
+    }
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+    row = "<div id='"+ Room_names + "' onclick = 'redirecttoroompage(this.id)'>#" + Room_names + "</div><hr>"
+    document.getElementById("output").innerHTML += row 
       //End code
       });});}
 getData();
+
+function redirecttoroompage(name){
+  localStorage.setItem("room_name", name)
+  window.location = "kwitter_page.html"
+} 
+
+
+
+
